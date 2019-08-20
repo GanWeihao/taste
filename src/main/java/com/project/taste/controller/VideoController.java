bda547fc5950f7e2c188e430d175015e1a9b0fd0
zyx <zyx172571@163.com>
2019-08-20 14:18:11 +0800

    根据用户ID查询该用户上传的所有视频

--- src/main/java/com/project/taste/controller/VideoController.java
+++ src/main/java/com/project/taste/controller/VideoController.java
@@ -5,6 +5,7 @@ import com.project.taste.model.Video;
 import com.project.taste.service.impl.VideoServiceImpl;
 import com.project.taste.util.Constants;
 import com.project.taste.util.JsonResult;
+import org.apache.ibatis.annotations.Param;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.CrossOrigin;
@@ -81,6 +82,31 @@ public class VideoController {
             if(video!=null){
                 result=new JsonResult(Constants.STATUS_SUCCESS,"查询成功",video);
             }else {
+                result=new JsonResult(Constants.STATUS_FAIL,"查询失败");
+            }
+        }catch(Exception e){
+            result=new JsonResult(Constants.STATUS_ERROR,"查询异常");
+        }
+        return result;
+    }
+
+    /**
+     * 根据用户id查询用户上传所有视频
+     * @param userId
+     * @param pageNum
+     * @param pageSize
+     * @return
+     */
+    @ResponseBody
+    @RequestMapping("/video/query/userid")
+    public Object queryVideoAllByid(@Param("userId") String userId,@Param("pageNum") Integer pageNum,@Param("pageSize") Integer pageSize){
+        JsonResult result=null;
+        try{
+            PageHelper.startPage(pageNum,pageSize);
+            List<Video> list = videoService.queryVideoAllByid(userId);
+            if(list.size()>0){
+                result=new JsonResult(Constants.STATUS_SUCCESS,"查询成功",list);
+            }else{
                 result=new JsonResult(Constants.STATUS_FAIL,"查询失败");
             }
         }catch(Exception e){
