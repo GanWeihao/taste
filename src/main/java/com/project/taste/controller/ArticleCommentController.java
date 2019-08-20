278919b4bf2fedfbd3a83c150d0a8dfbe43f6a2f
zm7081216004 <zhangmin123_45678@163.com>
2019-08-20 14:15:53 +0800

    根据用户ID查文章评论

--- src/main/java/com/project/taste/controller/ArticleCommentController.java
+++ src/main/java/com/project/taste/controller/ArticleCommentController.java
@@ -35,4 +35,23 @@ public class ArticleCommentController {
         }
         return result;
     }
+    //根据用户ID查询文章评论
+    @ResponseBody
+    @RequestMapping("/articlecomment/query/userid")
+    public Object queryUserByIdComment(String articleCommentUserId){
+        JsonResult result=null;
+        try{
+            List<ArticleComment> list=articleCommentService.queryUserByIdComment(articleCommentUserId);
+            if(list.size()>0){
+                result=new JsonResult(Constants.STATUS_SUCCESS,"查询成功",list);
+            }else {
+                result = new JsonResult(Constants.STATUS_FAIL, "查询失败");
+            }
+        }catch(Exception e){
+            e.printStackTrace();
+            result=new JsonResult(Constants.STATUS_ERROR,"查询异常");
+        }
+        return result;
+    }
+
 }
